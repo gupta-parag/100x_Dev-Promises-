4626f5f47b0c7fd665f8a9ad271fbf8bb82e0e3e
gupta-parag <git@stackblitz.com>
2025-12-31 18:53:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 100x_Dev-Promises-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gupta-parag/100x_Dev-Promises-)
---
+++ index.js
@@ -0,0 +1,14 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+const fs = require('fs');
+
+fs.readFile('a.txt', 'utf-8', function (err, data) {
+  console.log(data);
+});
+
+console.log('Hello - 1');
+
+setInterval(function () {
+  console.log('Hello from Timeout_1');
+}, 1000);
