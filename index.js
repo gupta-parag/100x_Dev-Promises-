5cab971a077c4da8e1e1716ec08a60c556ed491e
gupta-parag <paragupta03@gmail.com>
2025-12-31 19:00:47 -0500

    Added C.txt file

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ setInterval(function () {
 fs.readFile("b.txt", "utf-8", function(err,data){
   console.log(data)
 })
+
+
+fs.readFile("c.txt", "utf-8", function(err,data){
+  console.log(data)
+})
